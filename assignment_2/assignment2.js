6f180f437dd7619df277313277a6ac4b7db377d8
Sarmilan Somasundaram <Milan@Sarmilans-MacBook-Air.local>
2017-03-27 13:22:51 -0400

    first code push

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,6 +3,9 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
+for (i=1; i<101; i++) {
+	console.log(i);
+}
 
 
 /*
@@ -17,3 +20,10 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+var hash = '';
+
+while (hash.length < 7) {
+	hash += '#';
+	console.log (hash);
+}
